a5c27aa74142c3b6ec691fe6d75cf81ca6870b89
Harsha <hsenaratne@wabtec.com>
2021-06-20 04:53:22 +0530

    added push notification token saving to shared preferences

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,22 +1,78 @@
 import 'package:centicbids/src/screens/home/home.dart';
 import 'package:centicbids/src/screens/login/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   final auth = FirebaseAuth.instance;
 
+  String pushToken = '';
 
-  // Widget validateUser(){
-  //   if(auth.currentUser?.uid !=null){
-  //     return HomeBlocProvider();
-  //   }
-  //   else{
-  //     return HomeBlocProvider();
-  //
-  //   }
-  // }
+  String _homeScreenText = "Waiting for token...";
+  String _messageText = "Waiting for message...";
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        setState(() {
+          _messageText = "Push Messaging message: $message";
+        });
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Push Messaging token: $token";
+        pushToken = token;
+      });
+      asyncFunc();
+    });
+  }
+
+  asyncFunc() async { // Async func to handle Futures easier; or use Future.then
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.setString('pushToken', pushToken);
+
+    String token = prefs.getString('pushToken');
+
+    print("myyyyyyyyyyy tokennn$token");
+    
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
